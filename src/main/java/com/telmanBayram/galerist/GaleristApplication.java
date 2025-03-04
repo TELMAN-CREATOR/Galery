e79d4e03a68d54cd249a1f0ba802a754cfa2baa8
Telman Bayram <telmanbayram1@gmail.com>
2025-03-04 18:48:45 +0400

    Start to create new project

---
+++ src/main/java/com/telmanBayram/galerist/GaleristApplication.java
@@ -0,0 +1,19 @@
+package com.telmanBayram.galerist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EntityScan(basePackages = "com.telmanBayram")
+@ComponentScan(basePackages = "com.telmanBayram")
+@EnableJpaRepositories(basePackages = "com.telmanBayram")
+@SpringBootApplication
+public class GaleristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaleristApplication.class, args);
+	}
+
+}
